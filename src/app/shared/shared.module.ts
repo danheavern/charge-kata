284f9a5ae98710c24745c6f995de64a0e43f4192
danheavern <danheavern@gmail.com>
2020-02-16 16:25:42 -0500

    feat(home): add item list component to the home template

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { LoaderComponent } from './loader/loader.component';
+import { ItemListComponent } from '@app/item-list/item-list.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [LoaderComponent],
-  exports: [LoaderComponent]
+  declarations: [LoaderComponent, ItemListComponent],
+  exports: [LoaderComponent, ItemListComponent]
 })
 export class SharedModule {}
