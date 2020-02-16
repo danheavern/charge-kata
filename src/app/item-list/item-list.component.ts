a22362341323f183e06b7aa767d8797f3d809ec1
danheavern <danheavern@gmail.com>
2020-02-16 16:24:39 -0500

    feat(item-list): initialize item-list component
    The item list component will be where the list of items will appear

---
+++ src/app/item-list/item-list.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-list',
+  templateUrl: './item-list.component.html',
+  styleUrls: ['./item-list.component.scss']
+})
+export class ItemListComponent implements OnInit {
+  columnHeaders = [
+    'Location',
+    'Item Number',
+    'Date',
+    'Description'
+  ];
+  testData=[
+    {
+      location: 'location',
+      itemNumber: 1,
+      date: '02/16/2020',
+      description: 'this is test data'
+    },
+    {
+      location: 'location',
+      itemNumber: 2,
+      date: '02/16/2020',
+      description: 'this is test data'
+    },
+    {
+      location: 'location',
+      itemNumber: 3,
+      date: '02/16/2020',
+      description: 'this is test data'
+    },
+    {
+      location: 'location',
+      itemNumber: 4,
+      date: '02/16/2020',
+      description: 'this is test data'
+    },
+    {
+      location: 'location',
+      itemNumber: 5,
+      date: '02/16/2020',
+      description: 'this is test data'
+    }
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
+
+}
