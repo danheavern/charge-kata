ee2f8ebba09243873195ad15bbe135836276be40
danheavern <danheavern@gmail.com>
2020-02-16 21:44:10 -0500

    feat(item-list): animate dropdown

--- src/app/item-list/item-list.component.ts
+++ src/app/item-list/item-list.component.ts
@@ -1,10 +1,31 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Item } from 'src/models/Item';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-item-list',
   templateUrl: './item-list.component.html',
-  styleUrls: ['./item-list.component.scss']
+  styleUrls: ['./item-list.component.scss'],
+  animations: [
+    trigger('openClose', [
+      state(
+        'open',
+        style({
+          height: '60px',
+          opacity: 1
+        })
+      ),
+      state(
+        'closed',
+        style({
+          height: '0px',
+          opacity: 0
+        })
+      ),
+      transition('open => closed', [animate('0.07s')]),
+      transition('closed => open', [animate('0.1s')])
+    ])
+  ]
 })
 export class ItemListComponent implements OnInit {
   @Input() items: Item[];
