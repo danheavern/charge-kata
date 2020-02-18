908e9918386992311cf72ab8ea2c284f9c98b2d7
danheavern <danheavern@gmail.com>
2020-02-17 23:48:53 -0500

    feat(are-you-sure): init component for delete modal

---
+++ src/app/are-you-sure/are-you-sure.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-are-you-sure',
+  templateUrl: './are-you-sure.component.html',
+  styleUrls: ['./are-you-sure.component.scss']
+})
+export class AreYouSureComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
