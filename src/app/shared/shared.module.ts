dda27bc81c9ebc0fe0ebf8301ba8fc4fd04fc487
danheavern <danheavern@gmail.com>
2020-02-17 22:22:04 -0500

    feat(barcode): init barcode component
    barcode component will likewise componetize the barcode feature of the item

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,10 +6,11 @@ import { LoaderComponent } from './loader/loader.component';
 import { ItemListComponent } from '@app/item-list/item-list.component';
 import { NewItemFormComponent } from '@app/new-item-form/new-item-form.component';
 import { ItemComponent } from '@app/item/item.component';
+import { BarcodeComponent } from '@app/barcode/barcode.component';
 
 @NgModule({
   imports: [CommonModule, ReactiveFormsModule],
-  declarations: [LoaderComponent, ItemListComponent, NewItemFormComponent, ItemComponent],
+  declarations: [LoaderComponent, ItemListComponent, NewItemFormComponent, ItemComponent, BarcodeComponent],
   exports: [LoaderComponent, ItemListComponent, NewItemFormComponent]
 })
 export class SharedModule {}
